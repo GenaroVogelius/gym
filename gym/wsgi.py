fbce7fed126ff3b5b986acba9866577816e15655
Genaro Vogelius <genagevo@gmail.com>
2023-05-19 09:48:42 -0300

    wsgi

--- gym/wsgi.py
+++ gym/wsgi.py
@@ -10,9 +10,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from gym.settings.base import Settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", Settings)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gym.settings.base")
 
 application = get_wsgi_application()
 
