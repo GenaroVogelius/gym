6b5a3e689254be3665dcb47e4a27cb2ff3187a15
Genaro Vogelius <genagevo@gmail.com>
2023-05-21 19:46:43 -0300

    changes

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-ls
+
+export REACT_APP_RENDER_GIT_COMMIT=$RENDER_GIT_COMMIT
+yarn build
+
 python manage.py collectstatic --no-input
 python manage.py migrate
--- gym/settings.py
+++ gym/settings.py
@@ -137,8 +137,7 @@ STATICFILES_DIRS = [
     
 ]
 
-print(os.path.join(BASE_DIR, 'front-end-power/dist/assets'))
-print(BASE_DIR)
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
