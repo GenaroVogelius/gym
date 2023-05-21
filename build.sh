a6e3609affe6231a62abda8f43b630f8c5b98136
Genaro Vogelius <genagevo@gmail.com>
2023-05-21 19:50:27 -0300

    changes

--- build.sh
+++ build.sh
@@ -4,8 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-export REACT_APP_RENDER_GIT_COMMIT=$RENDER_GIT_COMMIT
-yarn build
-
 python manage.py collectstatic --no-input
 python manage.py migrate
