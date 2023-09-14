b519fd860b4d540f70d4fbb2cc01d4b3175341b6
Genaro Vogelius <genagevo@gmail.com>
2023-09-14 12:08:36 -0300

    changes in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 
 set -o errexit #This command sets the option errexit or e, which means that the script will exit immediately if any command returns a non-zero exit status (i.e., encounters an error). This ensures that if any of the subsequent commands fail, the script will stop executing further.
 
-source /home/vps-3503468-x.dattaweb.com/public_html/bin/activate
+source /home/power-gym.com.ar/public_html/bin/activate
+
+git pull
 
 pip install -r requirements.txt
 
